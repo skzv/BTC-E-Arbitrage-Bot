9fa29af10ae7374fa0ab6c51dee0742c77bf3240
Alexander Kuznetsov <alexander.kuznetsov96@gmail.com>
2017-03-26 02:44:22 -0400

    progress

--- btc-e-arbbot-V14/BTC-E ArbBot/BTC-E ArbBot/BTC-E ArbBot.cpp
+++ btc-e-arbbot-V14/BTC-E ArbBot/BTC-E ArbBot/BTC-E ArbBot.cpp
@@ -16,6 +16,7 @@
 #include <sstream>
 #include <algorithm>
 #include "split.h"
+#include <limits>
 
 
 using namespace rapidjson;
@@ -46,8 +47,8 @@ int main()
 		cur_tokens = split(currency_pair, delim);
 		for (auto i = cur_tokens.begin(); i != cur_tokens.end(); ++i) {
 			if (!currency_map.count((*i))) {
-				count++;
 				currency_map.insert(std::pair<std::string, int>((*i), count));
+				count++;
 			}
 		}
 	}
@@ -59,49 +60,145 @@ int main()
 
 	///* Let us create the graph */
 	int V = currency_map.size();  // Number of vertices in graph
-	int E = num_pairs*2;  // Number of edges in graph, 2 because bi-directional
-	struct Graph* graph = createGraph(V, E);
-	int edge = 0;
+	double** w = new double*[V];
+	for (int i = 0; i < V; ++i)
+		w[i] = new double[V];
 
-	for (Value::ConstMemberIterator itr = pairs.MemberBegin(); itr != pairs.MemberEnd(); ++itr)
-	{
-		num_pairs++;
-		bool first = true;
-		currency_pair = itr->name.GetString();
-		const Value& pair_value = pairs[currency_pair.c_str()];
-		//get ticker
-		json_data ticker = api.ticker(btc_e::pair(currency_pair));
-		Document t;
-		t.Parse(ticker.c_str());
-		Value& ticker_v = t["ticker"];
-		float sell = ticker_v["sell"].GetFloat();
-		float buy = ticker_v["buy"].GetFloat();
-		float fee = pair_value["fee"].GetFloat();
+	//initialize to inf
+	for (int k = 0; k < V; ++k)
+		for (int i = 0; i < V; ++i)
+			w[k][i] = DBL_MAX;
 
-		// add currencies
-		cur_tokens = split(currency_pair, delim);
-		for (auto i = cur_tokens.begin(); i != cur_tokens.end(); ++i) {
-			int cur_num = currency_map[(*i)];
-			if (first) {
-				// is src
-				graph->edge[edge].src = cur_num;
-				// set the buy price
-				graph->edge[edge].weight = -log(sell);
-				// set the sell price
-				graph->edge[edge+1].dest = cur_num;
-				graph->edge[edge+1].weight = -log(buy);
+	double** l = new double*[V];
+	for (int i = 0; i < V; ++i)
+		l[i] = new double[V];
+
+	//initialize to inf
+	for (int k = 0; k < V; ++k)
+		for (int i = 0; i < V; ++i)
+			l[k][i] = DBL_MAX;
+	
+	//while (true) {
+
+		for (Value::ConstMemberIterator itr = pairs.MemberBegin(); itr != pairs.MemberEnd(); ++itr)
+		{
+			num_pairs++;
+			currency_pair = itr->name.GetString();
+			const Value& pair_value = pairs[currency_pair.c_str()];
+			//get ticker
+			json_data ticker = api.ticker(btc_e::pair(currency_pair));
+			Document t;
+			t.Parse(ticker.c_str());
+			Value& ticker_v = t["ticker"];
+			double sell = ticker_v["sell"].GetDouble();
+			double buy = ticker_v["buy"].GetDouble();
+			double fee = pair_value["fee"].GetDouble();
+
+			// add currencies
+			cur_tokens = split(currency_pair, delim);
+			std::string cur1 = cur_tokens[0];
+			std::string cur2 = cur_tokens[1];
+			int cur1_num = currency_map[cur1];
+			int cur2_num = currency_map[cur2];
+
+			w[cur1_num][cur2_num] = (sell*(1 - fee / 100));
+			w[cur2_num][cur1_num] = 1 / (buy*(1 - fee / 100));
+			// transform
+			l[cur1_num][cur2_num] = -log(w[cur1_num][cur2_num]);
+			l[cur2_num][cur1_num] = -log(w[cur2_num][cur1_num]);
+			//printf("cur1: %d cur2: %d sell:%f buy:%f lns:%f lnb:%f\n", cur1_num, cur2_num, sell, buy, -log(sell*(1 - fee / 100)), -log(buy*(1 - fee / 100)));
+		}
+
+		// Calculate USD->LTC-BTC-USD
+		//printf("\nUSD->LTC->BTC->USD\n");
+		//float ulbu = w[1][4] * w[4][0] * w[0][1];
+		//printf("%f * %f * %f = %f\n", w[1][4], w[4][0], w[0][1], ulbu);
+		//float lulbu = l[1][4] + l[4][0] + l[0][1];
+		//float llulbu = -log(ulbu);
+		//printf("ulbu: %f lulbu: %f lllbu: %f\n", ulbu, lulbu, llulbu);
+
+		int src = 0;
+		printf("starting at %d\n", src);
+
+		//perform bellman ford
+		double* dis = new double[V];
+		int* pre = new int[V];
+
+		for (int i = 0; i < V; ++i)
+			dis[i] = DBL_MAX, pre[i] = -1;
+
+		dis[src] = 0;
+
+		for (int k = 0; k < V; ++k)
+			for (int i = 0; i < V; ++i)
+				for (int j = 0; j < V; ++j)
+					if (dis[i] + l[i][j] < dis[j])
+						dis[j] = dis[i] + l[i][j], pre[j] = i;
+
+		//for (int k = 0; k < V; ++k)
+		//	for (int i = 0; i < V; ++i)
+		//		if(w[k][i]!=DBL_MAX)
+		//			printf("w[%d][%d]=>%f\n",k,i,w[k][i]);
+
+		for (int k = 0; k < V; ++k)
+			printf("j=> %d\t dist=> %f\n", k, dis[k]);
+
+		// check for negative cycles
+		for (int i = 0; i < V; ++i)
+			for (int j = 0; j < V; ++j)
+				if (dis[i] + l[i][j] < dis[j]) {
+					//printf("please dear jesus \n");
+					// Node j is part of a negative cycle
+					printf("Node %d is part of a negative cycle\n", j);
+				}
+
+		//You can then use the pre array to find the negative cycles.
+		// Start with pre[source] and work your way back.
+
+		for (int k = 0; k < V; ++k)
+			printf("pre[%d]=>%d\n", k, pre[k]);
+
+		// start at src
+		std::vector<int> vector;
+		std::unordered_set<int> set;
+		float profit = 1;
+
+		int i = src;
+		while (true)
+		{
+			vector.push_back(i);
+			if (!set.count(i)) {
+				set.insert(i);
 			}
 			else {
-				// is dest
-				graph->edge[edge].dest = cur_num;
-				graph->edge[edge+1].src = cur_num;
-				edge=edge+2;
+				// pop until we find i again
+				bool first = true;
+				int c;
+				int b;
+				while (vector.size() > 0) {
+					if (first)
+					{
+						b = vector.back();
+						first = false;
+					}
+					else {
+						c = b;
+						b = vector.back();
+						printf(" %d <- %d | ", c, b);
+						profit *= w[b][c];
+					}
+					//printf("*[ %d (%s) ] \t", b, "tst");
+					vector.pop_back();
+				}
+				break;
 			}
-			first = false;
+			i = pre[i];
 		}
-	}
 
-	BellmanFord(graph, 0);
+		printf("\n\nprofit: %f", profit);
+		//Sleep(2 * 1000);
+	//}
+
 	getchar();
 	return 0;
 }
